bf7407ebca36797e47a90373262c4935668672e0
Benjamin Baraza <barazabenjamin58@gmail.com>
2025-11-03 16:08:04 +0300

    prisma-file added

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
     <div className='min-h-screen bg-background'>
       <Header/>
       <Hero/>
-      <HowItWorks/>
+      <HowItWorks/> 
       <WhatToAsk/>
       <PricingSection/>
       <CTA/>
